4700bbd9596301f0715a9cdafc426b7e39ba16bc
stromokocur5 <gabrielkohut3@proton.me>
2024-01-14 20:11:12 +0100

    Added some things from previous project

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use askama_axum::{IntoResponse, Template};
 use axum::Router;
 use tower_http::services::ServeDir;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct Index<'a> {
+    route: &'a str,
+    logged_in: bool,
+}
 
 #[tokio::main]
 async fn main() {
@@ -12,11 +20,20 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let app = Router::new().nest_service("/assets", ServeDir::new("assets"));
+    let app = Router::new()
+        .route("/", axum::routing::get(test))
+        .nest_service("/assets", ServeDir::new("assets"));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn test() -> impl IntoResponse {
+    Index {
+        route: "/login",
+        logged_in: false,
+    }
 }
