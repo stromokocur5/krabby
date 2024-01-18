846bc9d0ab5eaf107e79e1e2dd8cf36e4693dbbf
stromokocur5 <gabrielkohut3@proton.me>
2024-01-18 21:42:09 +0100

    uhm

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,7 @@ async fn main() {
 
     let app = Router::new().merge(krabby::router());
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
